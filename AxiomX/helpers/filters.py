1da5189d6c2e142d0af9d3770758fa455b77f877
The Maanav <prettylioness0550@gmail.com>
2026-05-31 12:05:26 +0530

    AxiomBots

---
+++ AxiomX/helpers/filters.py
@@ -0,0 +1,22 @@
+
+
+from AxiomX import BOT_ID, BOT_USERNAME
+from AxiomX.helpers.utils import search_text
+from AxiomX.db.chatbot import CHAT_IDS
+from telegram import constants
+from telegram.ext.filters import MessageFilter
+
+
+class ReplyToBot(MessageFilter):
+    def filter(self, message):
+        reply = message.reply_to_message
+        result = (
+        (message.text and 
+        ((message.from_user and reply and reply.from_user and reply.from_user.id == BOT_ID) 
+        or search_text(BOT_USERNAME, message.text) 
+        or search_text("(@|#)AxiomX", message.text)))
+        or message.sticker ) and message.chat.id in CHAT_IDS
+  
+        return result
+      
+ChatBotReply = ReplyToBot()
